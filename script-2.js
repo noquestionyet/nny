981d6787e4df64b55eeb6decfb8cc85352ef6116
noquestionyet <85129266+noquestionyet@users.noreply.github.com>
2022-12-01 13:57:25 +0300

    Update and rename script.js to script-2.js

--- script-2.js
+++ script-2.js
@@ -296,7 +296,9 @@ function showResult(sentToDb) {
 
     if (possiblePoints) {
         for (i = 0; i < possiblePoints.length; i++) {
+            console.log(possiblePoints[i])
             if (Number(possiblePoints[i].innerHTML) == Number(totalPoints)) {
+                console.log(possiblePoints[i])
                 const resultItem = $(possiblePoints[i]).closest(document.querySelector('[nny-quiz="result-item"]'));
                 resultItem.css({
                     "display": "block"
